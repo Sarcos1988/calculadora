a824271d17043fd6b2e8aa73d760dbb0a94970e9
Sarcos1988 <joseangelsarcos@gmail.com>
2017-06-28 21:10:31 -0300

    Calculadora Basica

---
+++ src/controlador/Logica.java
@@ -0,0 +1,153 @@
+package controlador;
+
+import Utils.Ayudante;
+import Utils.Division;
+import Utils.Multiplicacion;
+import Utils.Resta;
+import Utils.Suma;
+
+public class Logica {
+
+	public double calcular( String valores ){
+
+		String[] numeros;
+
+		double resultado = 0;
+
+		if( valores.contains( "+" ) ){
+
+			numeros = valores.split( "\\+" );
+
+			if( numeros.length > 1 ){
+
+				Suma sumar = new Suma();
+
+				switch ( numeros.length ) {
+
+				case 2:
+
+					resultado = sumar.Suma( "+" , Ayudante.conversor( numeros[0] ), Ayudante.conversor( numeros[1] ) );
+
+					break;
+
+				case 3:
+
+					resultado = sumar.Suma( "+" , Ayudante.conversor( numeros[0] ), Ayudante.conversor( numeros[1] ), Ayudante.conversor( numeros[2] ) );
+
+					break;
+
+				default:
+
+					resultado = sumar.Suma( "+" , Ayudante.conversor( numeros ) );
+
+					break;
+
+				}
+
+			}
+
+		}
+
+		if ( valores.contains( "-" ) ){
+
+			numeros = valores.split( "-" );
+
+			if( numeros.length > 1 ){
+
+				Resta restar = new Resta();
+
+				switch ( numeros.length ) {
+
+				case 2:
+
+					resultado = restar.Resta( "-" , Ayudante.conversor( numeros[0] ), Ayudante.conversor( numeros[1] ) );
+
+					break;
+
+				case 3:
+
+					resultado = restar.Resta( "-" , Ayudante.conversor( numeros[0] ), Ayudante.conversor( numeros[1] ), Ayudante.conversor( numeros[2] ) );
+
+					break;
+
+				default:
+
+					resultado = restar.Resta( "-" , Ayudante.conversor( numeros ) );
+
+					break;
+				}
+			}
+		}
+		
+		if ( valores.contains( "*" ) ){
+
+			numeros = valores.split( "\\*" );
+
+			if( numeros.length > 1 ){
+
+				Multiplicacion multiplicar = new Multiplicacion();
+
+				switch ( numeros.length ) {
+
+				case 2:
+
+					resultado = multiplicar.Multiplicacion( "*" , Ayudante.conversor( numeros[0] ), Ayudante.conversor( numeros[1] ) );
+
+					break;
+
+				case 3:
+
+					resultado = multiplicar.Multiplicacion( "*" , Ayudante.conversor( numeros[0] ), Ayudante.conversor( numeros[1] ), Ayudante.conversor( numeros[2] ) );
+
+					break;
+
+				default:
+
+					resultado = multiplicar.Multiplicacion( "*" , Ayudante.conversor( numeros ) );
+
+					break;
+
+				}
+
+			}
+
+		}
+		
+		if ( valores.contains( "/" ) ){
+
+			numeros = valores.split( "/" );
+
+			if( numeros.length > 1 ){
+
+				Division dividir = new Division();
+
+				switch ( numeros.length ) {
+
+				case 2:
+
+					resultado = dividir.Division( "/" , Ayudante.conversor( numeros[0] ), Ayudante.conversor( numeros[1] ) );
+
+					break;
+
+				case 3:
+
+					resultado = dividir.Division( "/" , Ayudante.conversor( numeros[0] ), Ayudante.conversor( numeros[1] ), Ayudante.conversor( numeros[2] ) );
+
+					break;
+
+				default:
+
+					resultado = dividir.Division( "/" , Ayudante.conversor( numeros ) );
+
+					break;
+
+				}
+
+			}
+
+		}
+
+		return resultado;
+
+	}
+}
